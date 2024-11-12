7d8bb782aaf2a91888857e8d9da07d0d82e3cd23
polemarkhos <polemarkhos>
2024-11-12 13:36:24 +0000

    add hashmap

---
+++ hashmap/hashmap.py
@@ -0,0 +1,22 @@
+class myHashMap:
+
+    def __init__(self):
+        self.buckets = [[] for _ in range(1000)]
+
+    def put(self, key: int, value: int) -> None:
+        bucket_index = key % 1000
+        if key not in self.buckets[bucket_index]:
+            self.buckets[bucket_index].extend([key, value])
+
+    def get(self, key: int) -> int:
+        bucket_index = key % 1000
+        if key in self.buckets[bucket_index]:
+            return 1
+        else:
+            return -1
+        
+    def remove(self, key: int) -> None:
+        bucket_index = key % 1000
+        if key in self.buckets[bucket_index]:
+            self.buckets[bucket_index] = []
+
