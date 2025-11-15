f759ff6adaf2385ddc512789027f3f6fa5d9c684
polemarkhos <snowmandevops@gmail.com>
2025-11-15 10:19:18 +0000

    more stuff

--- dynamicProgramming/climbingstairs.py
+++ dynamicProgramming/climbingstairs.py
@@ -20,3 +20,16 @@ def climbMemoized(num):
 
 print(climbStairs(5))
 print(climbMemoized(5))
+
+# bottom up
+def climbStairsBU(num):
+    if num <= 1:
+        return 1
+    # create a list of ways to climb n steps
+    # memoizing, but from the bottom up
+    dp = 0 * (num+1)
+    dp[0] = 1
+    dp[1] = 1
+    for i in range(2, num+1):
+        dp[i] = dp[i -1] + dp[i-2]
+    return dp[num]
